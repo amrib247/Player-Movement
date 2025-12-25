718d91eb5a625af648b9f5c772a847caf183e313
Amr Ibrahim <amr.ali.ibrahim247@gmail.com>
2025-12-25 15:57:47 -0800

    Basic Player Movement works

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,16 @@ public class PlayerMovement : MonoBehaviour
     
     public GameObject cam;
     public Rigidbody rb;
-    public float speed = 10f;
-    public float maxspeed = 10f;
+    public float speed = 50f;
+    public float airspeed = 10f;
+    public float maxspeed = 7f;
+    public float jump_power = 5f;
+    public float counterStrength = 15f;
+    public float groundCheckDistance = 2f;
+    public float groundCheckRadius = 0.3f;
+    public LayerMask groundMask;
     private Vector2 moveInput = Vector2.zero;
+    private bool grounded = true;
 
     void Start()
     {
@@ -18,20 +25,42 @@ public class PlayerMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext c)
     {
         moveInput = c.ReadValue<Vector2>();
-        //rb.AddForce(moveInput * speed, ForceMode.Impulse);
         Debug.Log(moveInput);
         Debug.Log(cam.transform.rotation.y);
     }
     public void OnJump(InputAction.CallbackContext c)
     {
-        if (c.performed)
+        if (c.performed && grounded)
         {
-            
+            rb.AddForce(Vector3.up * jump_power, ForceMode.Impulse);
         }
     }
+
     void FixedUpdate()
     {   
+        grounded = Physics.SphereCast(
+            transform.position + Vector3.up * 0.1f,
+            groundCheckRadius,
+            Vector3.down,
+            out RaycastHit hit,
+            groundCheckDistance,
+            groundMask,
+            QueryTriggerInteraction.Ignore
+        );
         Vector3 move = new Vector3(moveInput.y, 0f, -moveInput.x);
-        rb.AddForce(move * speed * (maxspeed - rb.linearVelocity.magnitude) / maxspeed, ForceMode.VelocityChange);
+        float angle = Vector3.SignedAngle(new Vector3(1f, 0f, 0f), new Vector3(cam.transform.forward.x, 0, cam.transform.forward.z), Vector3.up);
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        float velocity = (new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z)).magnitude;
+
+        //Player Movement
+        if (grounded) {
+            rb.AddForce(rotation * move * speed, ForceMode.Acceleration); //Move
+            //Counter Movement
+            rb.AddForce(Quaternion.AngleAxis(180f, Vector3.up) * (rotation * move) * counterStrength * velocity / maxspeed, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.AddForce(rotation * move * airspeed, ForceMode.Acceleration);
+        }
     }
 }
